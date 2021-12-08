949d8cebfc984c9cb56c19fa4e473588de50ee17
chelleorc <chelleorc@yahoo.com>
2021-12-08 18:45:29 -0500

    remove 'forces' property (no pbc) and add k-points

--- ase-pyscf/ase-pyscf-test.py
+++ ase-pyscf/ase-pyscf-test.py
@@ -4,23 +4,12 @@ Take ASE Diamond structure, input into PySCF and run
 
 import pyscf.pbc.gto as pbcgto
 import pyscf.pbc.dft as pbcdft
-from pyscf import lib, gto, scf, dft, mp, cc, ci
+from pyscf.pbc import scf
 
 from ase.calculators.PYSCF_calculator import ase_atoms_to_pyscf as pyscf_atom
-from ase.calculators.PYSCF_calculator import PySCF
+from ase.calculators.PYSCF_calculator import PySCF, make_kpts
 from ase.lattice.cubic import Diamond
 
-class parameters():
-    # holds the calculation mode and user-chosen attributes of post-HF objects
-    def __init__(self):
-        self.mode = 'hf'
-    def show(self):
-        print('------------------------')
-        print('calculation-specific parameters set by the user')
-        print('------------------------')
-        for v in vars(self):
-            print('{}:  {}'.format(v,vars(self)[v]))
-        print('\n\n')
 
 # Use cubic diamond structure from ASE and
 # calculate its volume
@@ -36,28 +25,19 @@ molcell.a = ase_atom.cell # initialize crystal shape using lattice vectors
 molcell.basis = 'gth-szv' # initialize basis
 molcell.pseudo = 'gth-pade' # initialize pseudopotential
 
-
 ### Set up calculation ###
 mf_class = pbcdft.RKS # --> mf_class
 mf_dict = {'xc':'lda,vwn'} # -->mf_dict
+args = {'molcell': molcell,'mf_class': mf_class, 'mf_dict': mf_dict}
 
-index = 4
-
-mf_p = parameters()
-mf_p.mode = ['hf','mp2','cisd','ccsd','ccsd(t)'][index]
-mf_p.verbose = 5
-mf_p.show()
-
-molcell.verbose = mf_p.verbose
-
-args = {'molcell': molcell,'mf_class': mf_class, 'mf_dict': mf_dict,'mf_p': mf_p}
-
-
-
-### Call PySCF class for access to functions ###
-# calc = PySCF(molcell=molcell, mf_class=mf_class, mf_dict=mf_dict)
+#  Set up for k-point sampling
+nk = [5,5,5]
+kpts =molcell.make_kpts(nk)
 
 calc = PySCF(**args)
 calc.calculate(atoms=ase_atom)
+print(kpts.shape)
+
+
 
 
--- ase-pyscf/ase/ase/calculators/PYSCF_calculator.py
+++ ase-pyscf/ase/ase/calculators/PYSCF_calculator.py
@@ -6,8 +6,7 @@ electronic structure calculations.
 '''
 import numpy as np
 from ase.calculators.calculator import (Calculator, CalculatorError, 
-                    CalculatorSetupError, all_changes, all_properties)
-
+                    CalculatorSetupError, all_changes, all_properties,kpts2m)
 
 
 def ase_atoms_to_pyscf(ase_atoms):
@@ -21,7 +20,7 @@ atoms_from_ase = ase_atoms_to_pyscf
 
 
 class PySCF(Calculator):
-    implemented_properties = ['energy','forces']
+    implemented_properties = ['energy']
 
     def __init__(self, restart=None, ignore_bad_restart_file=False,
                  label='PySCF', atoms=None, scratch=None, **kwargs):
@@ -80,54 +79,10 @@ class PySCF(Calculator):
             self.mf.__dict__[key] = self.mf_dict[key]
 
         self.results['energy']=self.mf.scf()
-        self.results['mf']=self.mf
-        
-        if th == 'mp2': # Need to fix this
-            self._ham = mp.MP2(self._mf)
-            hamkwargs = p['mp']
-        elif th == 'ccsd':
-            self._ham = cc.CCSD(self._mf)
-            hamkwargs = p['cc']
-        elif th == 'cisd':
-            self._ham = ci.CISD(self._mf)
-            hamkwargs = p['ci']
-        else:
-            self._ham = self._mf
-            hamkwargs = dict()
 
-        # Set Hamiltonian arguments
-        # Is this the best way to do this?
-        for key, val in hamkwargs.items():
-            setattr(self._ham, key, val)
-
-        # Energy/gradient scanner for more efficient evaluations
-        self._escanner = None
-        self._gscanner = None
-
-        if 'forces' in properties:
-            if self._gscanner is None:
-                self._gscanner = self._ham.nuc_grad_method().as_scanner()
-            e, g = self._gscanner(self._mol)
-            self.results['forces'] = -(g * Hartree / Bohr).reshape((-1, 3))
-        else:
-            if self._escanner is None:
-                self._escanner = self._ham.as_scanner()
-            e = self._escanner(self._mol)
-        '''  
-        if 'forces' in properties:
-            gf = self.mf.nuc_grad_method()
-            gf.verbose = self.mf.verbose
-            if self.p.mode.lower() == 'dft':
-                gf.grid_response = True
-            forces = -1. * gf.kernel() * (Ha / Bohr)
-            totalforces = []
-            totalforces.extend(forces)
-            totalforces = numpy.array(totalforces)
-            self.results['forces'] = totalforces
-        '''
-        self.results['energy'] = e * Hartree
-
+# K-point sampling
 def make_kpts(cell, nks):
     raise DeprecationWarning('Use cell.make_kpts(nks) instead.')
+
 
 
